a05f4da8ba366b5968775364e0b1197b336bf06d
colorbox <colorbox222@gmail.com>
2017-12-22 23:31:30 +0900

    Add entry model

---
+++ db/migrate/20171222133852_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries do |t|
+      t.string :title, null: false
+      t.string :url, null: false
+      t.string :category, null: false
+
+      t.timestamps
+    end
+  end
+end
