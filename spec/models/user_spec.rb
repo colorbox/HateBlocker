4bf2ffc24e91376bda6595c135702ac4d2d5d65e
colorbox <colorbox222@gmail.com>
2017-12-29 21:24:24 +0900

    Add user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'prohibit entry' do
+    let(:category){ create(:category) }
+    let!(:entry){ create(:entry, title: 'title', url: 'http://example.com', category: category) }
+    let(:user){ create(:user) }
+    let!(:prohibition){ create(:title_prohibition, word: 'title', user: user) }
+
+    subject { user.prohibit_entry?(entry) }
+
+    it { is_expected.to eq true }
+
+    context 'prohibit with domain' do
+      let!(:prohibition){ create(:domain_prohibition, word: 'example', user: user) }
+
+      it { is_expected.to eq true }
+    end
+
+    context 'not prohibit' do
+      let!(:prohibition){ create(:title_prohibition, word: 'invalid_title', user: user) }
+
+      it { is_expected.to eq false }
+
+      context 'with domain' do
+        let!(:prohibition){ create(:domain_prohibition, word: 'invalid_example', user: user) }
+
+        it { is_expected.to eq false }
+      end
+    end
+
+    context 'multi prohibit' do
+      let!(:prohibition){ create(:title_prohibition, word: 'invalid_title', user: user) }
+      let!(:second_prohibition){ create(:domain_prohibition, word: 'invalid_example', user: user) }
+
+      it { is_expected.to eq false }
+    end
+  end
 end
