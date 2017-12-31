454cb46a7e8d1b1fc4b06f1093626998bba03cc3
colorbox <colorbox222@gmail.com>
2017-12-31 22:35:17 +0900

    Refactor login button related before_actions

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,14 +3,9 @@ require 'json'
 
 class EntriesController < ApplicationController
   before_action :set_user
+  before_action :login_or_signup
 
   def index
     @entries = Category.find_by(kind: params[:category_kind]).entries.reject{|entry| @user.prohibit_entry?(entry)}
-  end
-
-  private
-
-  def set_user
-    @user = User.where(identifier: session[:user_identifier], access_token: session[:access_token]).first
   end
 end
