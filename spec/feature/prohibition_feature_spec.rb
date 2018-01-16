6f1ff321e6f0d9e6245c6819b7182bd1304a1099
colorbox <colorbox222@gmail.com>
2018-01-17 00:56:14 +0900

    Add prohibition add test

--- spec/feature/prohibition_feature_spec.rb
+++ spec/feature/prohibition_feature_spec.rb
@@ -12,4 +12,15 @@ RSpec.feature 'Prohibition index', type: :feature do
 
     expect(page).to have_content('ブロック要素の追加')
   end
+
+  scenario 'add domain prohibition' do
+    visit prohibitions_path
+
+    fill_in :input_word, with: 'test_domain'
+
+    click_button 'add'
+
+    expect(Prohibition.first.word).to eq('test_domain')
+    expect(Prohibition.first.prohibition_type).to eq('domain')
+  end
 end
