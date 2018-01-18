10e647c2533be3be36db00c92566d485f947ec8c
colorbox <colorbox222@gmail.com>
2018-01-18 16:34:26 +0900

    Add title prohibition

--- spec/feature/prohibition_feature_spec.rb
+++ spec/feature/prohibition_feature_spec.rb
@@ -16,11 +16,26 @@ RSpec.feature 'Prohibition index', type: :feature do
   scenario 'add domain prohibition' do
     visit prohibitions_path
 
+    select 'URLの一部', from: 'prohibition_prohibition_type'
+
     fill_in :input_word, with: 'test_domain'
 
     click_button 'add'
 
     expect(Prohibition.first.word).to eq('test_domain')
     expect(Prohibition.first.prohibition_type).to eq('domain')
+  end
+
+  scenario 'add title prohibition' do
+    visit prohibitions_path
+
+    select 'タイトルの一部', from: 'prohibition_prohibition_type'
+
+    fill_in :input_word, with: 'test_word'
+
+    click_button 'add'
+
+    expect(Prohibition.first.word).to eq('test_word')
+    expect(Prohibition.first.prohibition_type).to eq('title')
   end
 end
